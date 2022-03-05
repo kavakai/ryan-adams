8f952e6dc8bd66df48aaf8ec63d6e9754a6ac785
Kai Kuller <kavakai@gmail.com>
2022-03-05 07:02:01 -0700

    Add user flow tests for main page

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -1,20 +1,37 @@
 describe("home page", () => {
 
   beforeEach(() => {
+    // cy.intercept("http://localhost:4020/", { fixture: "testData" }).as("testData")
     cy.visit("http://localhost:3000/")
-      .intercept("http://localhost:4020", { fixture: "testData.json" })
   })
   
-  it("I should see a title/navgation button and other navigation buttons in the header", () => {
+  it("I should see a title/navgation button and other navigation buttons in the header and all album covers", () => {
     cy.get("[data-testid=nav-buttons]")
       .should("have.length", 3)
       .first()
       .contains("Ryan Adams")
-  });
-
-  it("Should display all album covers", () => {
-    cy.get("[data-testid=cover-img]")
+      .get("[data-testid=cover-img]")
       .should("have.length", 19)
       .should("have.class", "cover-link")
   });
+
+  it("Should be able to click 'playlist' and see my playlist", () => {
+    cy.get("[data-testid=nav-buttons]")
+      .eq(1)
+      .click()
+      .server()
+      .route("/playlist")
+      .get("h2")
+      .contains("Add some albums and let's start listening")
+  });
+
+  it("Should be able to click 'about' and see a paragraph about Ryan", () => {
+    cy.get("[data-testid=nav-buttons]")
+      .eq(2)
+      .click()
+      .server()
+      .route("/about")
+      .get("p")
+      .should("have.class", "about-content")
+  })
 })
