9b912d64f5d4e0e53a79f7b430da0f397d934c08
Kai Kuller <kavakai@gmail.com>
2022-03-05 20:23:58 -0700

    Add error tests

--- cypress/integration/single-album_spec.js
+++ cypress/integration/single-album_spec.js
@@ -5,17 +5,17 @@ describe("Single album view", () => {
     cy.visit("http://localhost:3000/")
   })
 
-  // it("Should be able to view info on my selected album", () => {
-  //   cy.get("[data-testid=cover-img]")
-  //     .first()
-  //     .click()
-  //     .server()
-  //     .route("/album/1")
-  //     .get("h2")
-  //     .contains("Heartbreaker")
-  //     .get("p")
-  //     .should("have.length", 18)
-  // });
+  it("Should be able to view info on my selected album", () => {
+    cy.get("[data-testid=cover-img]")
+      .first()
+      .click()
+      .server()
+      .route("/album/1")
+      .get("h2")
+      .contains("Heartbreaker")
+      .get("p")
+      .should("have.length", 18)
+  });
 
   it("Should be able to add the album to my playlist", () => {
     cy.get("[data-testid=cover-img]")
@@ -27,6 +27,29 @@ describe("Single album view", () => {
       .click()
       .get("h3")
       .contains("You have added this album to your playlist")
-  })
+  });
 
+  it("Should tell me if I have already added an album to my playlist", () => {
+    cy.get("[data-testid=cover-img]")
+      .first()
+      .click()
+      .server()
+      .route("/album/1")
+      .get("button")
+      .click()
+      .get("h3")
+      .contains("You have added this album to your playlist")
+      .get("a")
+      .first()
+      .click()
+      .get("[data-testid=cover-img]")
+      .first()
+      .click()
+      .server()
+      .route("/album/1")
+      .get("button")
+      .click()
+      .get("h3")
+      .contains("Album already in your playlist")
+  });
 })
