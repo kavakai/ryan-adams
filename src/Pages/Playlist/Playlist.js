b68b129e4eb409a8e760b0ddb799266e22615411
Kai Kuller <kavakai@gmail.com>
2022-03-05 21:15:21 -0700

    Add prop-types to playlist page

--- src/Pages/Playlist/Playlist.js
+++ src/Pages/Playlist/Playlist.js
@@ -1,22 +1,25 @@
-import React, { useContext } from "react"
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
-import { RyanContext } from "../../Contexts/RyanContext"
-import AlbumContainer from "../../Components/AlbumContainer"
-import PlaylistContainer from "../../Components/PlaylistContainer"
+import { RyanContext } from "../../Contexts/RyanContext";
+import PlaylistContainer from "../../Components/PlaylistContainer";
 
 const Playlist = () => {
 
-  const { playlist } = useContext(RyanContext)
+  const { playlist } = useContext(RyanContext);
 
   return (
-    <main className='main'>  
+    <main className='main'>
       <PlaylistContainer albums={playlist} />
     </main>
   )
-}
+};
 
 export default Playlist
 
 Playlist.propTypes = {
   albums: PropTypes.array.isRequired
-}
+};
+
+Playlist.defaultProps = {
+  albums: []
+};
